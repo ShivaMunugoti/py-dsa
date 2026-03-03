427f03f4b3a947497b08db89ad86a923cb3da6cf
shiva <munugotishivasai123@gmail.com>
2026-03-03 18:36:42 +0530

    Initial commit

---
+++ 5qmonth.py
@@ -0,0 +1,32 @@
+def getMonthName(monthNumber):
+    if monthNumber == 1:
+        print("January")
+    elif monthNumber == 2:
+        print("February")
+    elif monthNumber == 3:
+        print("March")
+    elif monthNumber == 4:
+        print("April")
+    elif monthNumber == 5:   
+        print("May")
+    elif monthNumber == 6:          
+        print("June")
+    elif monthNumber == 7:
+        print("July")
+    elif monthNumber == 8:
+        print("August")
+    elif monthNumber == 9:
+        print("September")
+    elif monthNumber == 10:
+        print("October")
+    elif monthNumber == 11:
+        print("November")
+    elif monthNumber == 12:
+        print("December")
+    else:
+        print("Invalid input!")
+    
+num = int(input())
+getMonthName (num)
+## example input is = 11
+## the output is november
